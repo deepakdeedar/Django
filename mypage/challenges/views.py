18ca5508d91eef750740792094f91f80a1efda54
Deepak Deedar Singla <ddeedar.cse18@chitkarauniversity.edu.in>
2021-05-19 23:11:55 +0530

    views and urls added

--- mypage/challenges/views.py
+++ mypage/challenges/views.py
@@ -1,6 +1,47 @@
+import challenges
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+monthly_challenges = {
+    "january": "Eat no meat for the entire month!",
+    "february": "Walk for at least 20 minutes every day!",
+    "march": "Learn Django for at least 20 minutes every day!",
+    "april": "Eat no meat for the entire month!",
+    "may": "Walk for at least 20 minutes every day!",
+    "june": "Learn Django for at least 20 minutes every day!",
+    "july": "Eat no meat for the entire month!",
+    "august": "Walk for at least 20 minutes every day!",
+    "september": "Learn Django for at least 20 minutes every day!",
+    "october": "Eat no meat for the entire month!",
+    "november": "Walk for at least 20 minutes every day!",
+    "december": "Learn Django for at least 20 minutes every day!"
+}
+
 def index(request):
-    return HttpResponse('Hello World!')
+    list_item = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        month_path = reverse("month-challenge", args=[month])
+        list_item += f"<li><a href='{month_path}'>{month.capitalize()}</a></li>"
+    
+    return HttpResponse(f"<ul>{list_item}</ul>")
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        return HttpResponseNotFound("Invalid Path")
+
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("Please enter a valid month.")
--- mypage/mypage/settings.py
+++ mypage/mypage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^mbgq1k&uk*o93u9o2(+wjcbl^w=nxak3%&l+b44nptegkc5&m'
+SECRET_KEY = 'xi$rs6684@qp^3^o*afn)#^biu#51j2povk9iie*0z$@(w3-p-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
