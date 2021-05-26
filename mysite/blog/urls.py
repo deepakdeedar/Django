599d720273dce871aa11da2937114c4266e4c473
Deepak Deedar Singla <ddeedar.cse18@chitkarauniversity.edu.in>
2021-05-26 23:05:26 +0530

    frontend complete

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name='starting-page'),
-    path("/posts", views.posts, name='posts-page'),
-    path("/posts/<slug:slug>", views.post_detail, name='post-detail-page')
+    path("posts", views.posts, name='posts-page'),
+    path("posts/<slug:slug>", views.post_detail, name='post-detail-page')
 ]
