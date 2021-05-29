6656a9a35361b981819ad03c6440fdd8cd6cd919
Deepak Deedar Singla <ddeedar.cse18@chitkarauniversity.edu.in>
2021-05-29 18:45:56 +0530

    SQL added

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Book, Author
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ("author", "rating")
+    list_display = ("title", 'author')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -4,12 +4,23 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def fullname(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        return self.fullname()
+
 class Book(models.Model):
     title = models.CharField(max_length=50, unique=True)
     rating = models.IntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name='books')
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default='', null=False, db_index=True)
 
